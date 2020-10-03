3b4b51ac9875c2c64c2e4db26a8b60482ed94143
Amir Naghibi <amir.naghibi@visier.com>
2020-10-02 22:56:31 -0700

    Updates

---
+++ precache-manifest.b7bc3f2227685b319522b6ba026f1859.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f33718128ad77792b2493c73da1229",
+    "url": "/reactcontainerized/index.html"
+  },
+  {
+    "revision": "53b453730ee505a5dfe4",
+    "url": "/reactcontainerized/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "15d58133b11193409356",
+    "url": "/reactcontainerized/static/js/2.8994e4fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactcontainerized/static/js/2.8994e4fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b453730ee505a5dfe4",
+    "url": "/reactcontainerized/static/js/main.07b52a1f.chunk.js"
+  },
+  {
+    "revision": "a46f153fe116301b7815",
+    "url": "/reactcontainerized/static/js/runtime-main.cd64a6ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactcontainerized/static/media/logo.5d5d9eef.svg"
+  }
+]);
